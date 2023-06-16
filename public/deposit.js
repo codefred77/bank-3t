922dbe9854ca1de600803522132b3a34d58421d8
Fredarico E Dutton <fredonred@gmail.com>
2023-06-16 13:16:16 -0700

    Major recode and cleanup

--- public/deposit.js
+++ public/deposit.js
@@ -1,43 +1,71 @@
 function Deposit() {
-    const ctx = React.useContext(UserContext); 
-    const [status, setStatus] = React.useState(true);
+    const ctx = React.useContext(UserContext);
+    const [status, setStatus] = React.useState('');
+  
+    async function handleDeposit() {
 
-    function depositAmount() {
-        console.log ("Dep: Auth? " + ctx.auth);
-        if (ctx.user!=='') { 
-        console.log ("Dep: Auth? " + ctx.auth);
-        setStatus(`$${ctx.balance} deposit successful!`);
-        setTimeout(() => setStatus(''),2000);
-
-        ctx.balance.toString();
-        const url = `/account/deposit/${ctx.user}/${ctx.balance}`;
-        (async () => {
-            var res = await fetch(url);
-            var data = await res.json();
-            console.log(data);
-        })();
-        } else {
-            setStatus('Login to make a deposit');
-            setTimeout(() => setStatus(''),3000);
+      // The user is logged in; proceed with the deposit
+      if (ctx.user) {
+        console.log ("Deposit:" + ctx.balance);
+        // Check that the deposit amount is valid
+        if (ctx.balance < 0 || ctx.balance === null) {
+            setStatus ("Please enter positive numbers only");
+            setTimeout(() => setStatus(''), 2000);
+            return;
         }
-    }
+        // TBD - probably not needed since the form input type is 'number'
+        if (isNaN(ctx.balance)) {
+            setStatus ("Please enter numerical values only");
+            setTimeout(() => setStatus(''), 2000);
+            return;
+        }
 
+        try {
+          const response = await fetch(`/account/deposit/${ctx.user}/${ctx.balance}`);
+          const data = await response.json();
+          console.log(data);
+  
+          if (response.ok) {
+            setStatus(`$${ctx.balance} deposit successful!`);
+            setTimeout(() => setStatus(''), 2000);
+          } else {
+            setStatus('Deposit failed. Please try again.');
+            setTimeout(() => setStatus(''), 2000);
+          }
+        } catch (error) {
+          setStatus('An error occurred. Please try again later.');
+          setTimeout(() => setStatus(''), 2000);
+          console.error(error);
+        }
+
+      // No user logged in
+      } else {
+        setStatus('Login to make a deposit');
+        setTimeout(() => setStatus(''), 2000);
+      }
+    }
+  
+    // TBD - disable submit button if empty amount?
     return (
-        <Card
-            bgcolor="warning"
-            header="Deposit"
-            text=""
-            status={status}
-            body={
-                <>
-                <CardForm
-                    showName="none"
-                    showPassword="none"
-                    showEmail="none"
-                />
-                {<button type="submit" className="btn btn-light" onClick={depositAmount}>Deposit</button>}
-                </>
-            }
-        />
-    )
+      <Card
+        bgcolor="success"
+        header="Deposit"
+        text=""
+        status={status}
+        body={
+          <>
+            <CardForm 
+                showName="none" 
+                showPassword="none" 
+                showEmail="none" />
+            <button 
+                type="submit" 
+                className="btn btn-light" 
+                onClick={handleDeposit}>
+                Deposit
+            </button>
+          </>
+        }
+      />
+    );
 }
--- public/withdraw.js
+++ public/withdraw.js
@@ -1,41 +1,89 @@
 function Withdraw() {
-    const ctx = React.useContext(UserContext); 
-    const [status, setStatus]     = React.useState(true);
+    const ctx = React.useContext(UserContext);
+    const [status, setStatus] = React.useState('');
 
-    function withdrawAmount() {
-        if (ctx.user!=='') { 
-        setStatus(`$${ctx.balance} withdrawal successful!`);
-        setTimeout(() => setStatus(''),2000);
+    async function handleWithdrawal() {
 
-        ctx.balance.toString();
-        const url = `/account/withdraw/${ctx.email}/${ctx.balance}`;
-        (async () => {
-            var res = await fetch(url);
-            var data = await res.json();
+        // The user is logged in; proceed with the deposit
+        if (ctx.user) {
+          console.log ("Whitdraw:" + ctx.balance);
+          // Check that the withdraw amount is valid
+          if (ctx.balance < 0 || ctx.balance === null) {
+              setStatus ("Please enter positive numbers only");
+              setTimeout(() => setStatus(''), 3000);
+              return;
+          }
+          // TBD - probably not needed since the form input type is 'number'
+          if (isNaN(ctx.balance)) {
+              setStatus ("Please enter numerical values only");
+              setTimeout(() => setStatus(''), 3000);
+              return;
+          }
+
+          try {
+            const response = await fetch(`/account/withdraw/${ctx.user}/${ctx.balance}`);
+            const data = await response.json();
             console.log(data);
-        })();
+    
+            if (response.ok) {
+              setStatus(`$${ctx.balance} withdrawal successful!`);
+              setTimeout(() => setStatus(''), 3000);
+            } else {
+              setStatus('Withdrawal failed. Please try again.');
+              setTimeout(() => setStatus(''), 3000);
+            }
+          } catch (error) {
+            setStatus('An error occurred. Please try again later.');
+            setTimeout(() => setStatus(''), 3000);
+            console.error(error);
+          }
+  
+        // No user logged in
         } else {
-            setStatus('Login to make a withdrawal');
-            setTimeout(() => setStatus(''),3000);
+          setStatus('Login to make a withdrawal');
+          setTimeout(() => setStatus(''), 3000);
         }
     }
 
+    function withdrawAmount() {
+      if (ctx.user !== '') {
+        setStatus(`$${ctx.balance} withdrawal successful!`);
+        setTimeout(() => setStatus(''), 3000);
+  
+        const url = `/account/withdraw/${ctx.email}/${ctx.balance}`;
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error('Withdrawal failed:', error);
+          });
+      } else {
+        setStatus('Login to make a withdrawal');
+        setTimeout(() => setStatus(''), 3000);
+      }
+    }
+  
     return (
-        <Card
-            bgcolor="success"
-            header="Withdraw"
-            text=""
-            status={status}
-            body={
-                <>
-                <CardForm
-                    showName="none"
-                    showPassword="none"
-                    showEmail="none"
-                />
-                {<button type="submit" className="btn btn-light" onClick={withdrawAmount}>Withdraw</button>}
-                </>
-            }
-        />
-    )
+      <Card
+        bgcolor="danger"
+        header="Withdraw"
+        text=""
+        status={status}
+        body={
+          <>
+            <CardForm
+              showName="none"
+              showPassword="none"
+              showEmail="none"
+              handleBalanceChange={(e) => (ctx.balance = e.currentTarget.value)}
+            />
+            <button type="button" className="btn btn-light" onClick={handleWithdrawal}>
+              Withdraw
+            </button>
+          </>
+        }
+      />
+    );
 }
